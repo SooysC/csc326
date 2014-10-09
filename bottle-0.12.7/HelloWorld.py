49e49cefac694aa16353db9fb1310ab3872dacad
shirsho <srshirsho@gmail.com>
2014-10-09 00:20:32 -0400

    basic index.html file with search button and box, does nothing, not even form submission

--- bottle-0.12.7/HelloWorld.py
+++ bottle-0.12.7/HelloWorld.py
@@ -1,6 +1,10 @@
 from bottle import route, run
-@route('/')
+from bottle import static_file
 
+@route('/')
 def hello():
-  return "Hello World"
+  #return "<h1>Hello World<h1>"
+  filename = "index.html"
+  return static_file(filename, root='../html/')
 run(host='localhost', port=8080, debug=True)
+
