bb03fa0935f23b04d665866e3a85f8e7f9c33792
JonErikDSuero <jonerikdsuero@gmail.com>
2014-11-23 11:14:48 -0500

    crawler_db indentation

--- crawler/crawler_db.py
+++ crawler/crawler_db.py
@@ -1,30 +1,30 @@
 import sqlite3 as lite
 
 def connect_to_db(db_file = "../crawler/dbFile.db"):
-	con = lite.connect(db_file)
-	cur = con.cursor()
-	return con, cur
+    con = lite.connect(db_file)
+    cur = con.cursor()
+    return con, cur
 
 def get_doc_ids_from_db(con, cur, word):
-	cur.execute('SELECT InvertedIndex.doc_ids FROM Lexicon INNER JOIN InvertedIndex ON Lexicon.word_id = InvertedIndex.word_id WHERE Lexicon.word = "%s"' % word)
-	
-	try:
-		doc_ids = cur.fetchone()[0]
-	except:
-		doc_ids = []
-	return doc_ids
+    cur.execute('SELECT InvertedIndex.doc_ids FROM Lexicon INNER JOIN InvertedIndex ON Lexicon.word_id = InvertedIndex.word_id WHERE Lexicon.word = "%s"' % word)
+
+    try:
+        doc_ids = cur.fetchone()[0]
+    except:
+        doc_ids = []
+    return doc_ids
 
 def get_doc_urls_from_db(con, cur, doc_ids):
-	cur.execute('SELECT DocIndex.doc_url, PageRank.doc_rank FROM DocIndex LEFT JOIN PageRank ON DocIndex.doc_id = PageRank.doc_id WHERE DocIndex.doc_id IN (%s)' % doc_ids)
-	doc_urls = cur.fetchall()
-	sorted_doc_urls = sorted(doc_urls, key=lambda doc: doc[1], reverse=True)
-	return [url[0] for url in sorted_doc_urls ]
+    cur.execute('SELECT DocIndex.doc_url, PageRank.doc_rank FROM DocIndex LEFT JOIN PageRank ON DocIndex.doc_id = PageRank.doc_id WHERE DocIndex.doc_id IN (%s)' % doc_ids)
+    doc_urls = cur.fetchall()
+    sorted_doc_urls = sorted(doc_urls, key=lambda doc: doc[1], reverse=True)
+    return [url[0] for url in sorted_doc_urls ]
 
 def get_sorted_urls(word):
-	con, cur = connect_to_db()
+    con, cur = connect_to_db()
 
-	doc_ids = get_doc_ids_from_db(con, cur, word)
-	if doc_ids == []:
-		return []
-	else:
-		return get_doc_urls_from_db(con, cur, doc_ids)
+    doc_ids = get_doc_ids_from_db(con, cur, word)
+    if doc_ids == []:
+        return []
+    else:
+        return get_doc_urls_from_db(con, cur, doc_ids)
