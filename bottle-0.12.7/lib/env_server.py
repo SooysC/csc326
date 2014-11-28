fdf448256d3c9abf7f20db4d2e8c3353d5c65567
JonErikDSuero <jonerikdsuero@gmail.com>
2014-11-27 23:07:33 -0500

    google auth for multiple aws servers

--- bottle-0.12.7/lib/env_server.py
+++ bottle-0.12.7/lib/env_server.py
@@ -1,13 +1,15 @@
 import socket
 
-IP_ADDRESS_AWS_SERVER = '172.31.32.183'
+IP_ADDRESSES_FOR_AWS = ['172.31.32.183', '172.31.33.215']
 
+def current_ip():
+    return socket.gethostbyname(socket.gethostname())
 
 def is_aws():
-    return socket.gethostbyname(socket.gethostname()) == IP_ADDRESS_AWS_SERVER
+    return current_ip() in IP_ADDRESSES_FOR_AWS
 
 def client_secret_file():
     if is_aws():
-        return "client_secret_aws.json"
+        return "client_secret_%s.json" % current_ip()
     else:
         return "client_secret_localhost.json"
