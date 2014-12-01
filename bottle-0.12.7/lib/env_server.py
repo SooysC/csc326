cb4c271800c2c1e0b5f6abf77a0c0a692ade7310
JonErikDSuero <jonerikdsuero@gmail.com>
2014-12-01 07:20:10 -0500

    fixed is_aws()

--- bottle-0.12.7/lib/env_server.py
+++ bottle-0.12.7/lib/env_server.py
@@ -7,7 +7,7 @@ def current_ip():
     return socket.gethostbyname(socket.gethostname())
 
 def is_aws():
-    return len(get_instance_metadata().keys()) > 0
+    return len(get_instance_metadata(timeout=2, num_retries=2).keys()) > 0
 
 def client_secret_file():
     if current_ip() in IP_ADDRESSES_FOR_AWS:
